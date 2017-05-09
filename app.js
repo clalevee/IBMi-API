aad0d5f6b7e5e109ed441ab98b9350c7a9be4326
clalevee <clalevee@gmail.com>
2017-05-09 23:13:24 +0200

    modified:   .cfignore 	modified:   .gitignore 	modified:   app.js 	modified:   manifest.yml 	modified:   package.json 	new file:   public/images/ibmi.png 	modified:   public/images/newapp-icon.png 	modified:   public/index.html 	modified:   public/stylesheets/style.css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*eslint-env node*/
 
 //------------------------------------------------------------------------------
-// hello world app is based on node.js starter application for Bluemix
+// node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
@@ -21,9 +21,91 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+// Connect to DB2 for i ------------------------------------------------------------------
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+// DB2 for i driver
+var db = require('/QOpenSys/QIBM/ProdData/OPS/Node4/os400/db2i/lib/db2');
+
+db.init(function(){
+	db.serverMode(true); //Enable Server Mode if needed
 });
+
+console.log ("DB2 init done");
+db.conn("*LOCAL");
+console.log ("DB2 connect done");
+
+// ---------------------------------------------------------------------------------------
+
+app.get('/system/cpu', function(req, res, next) {
+    var result = {};
+    var sql = "SELECT elapsed_cpu_used FROM QSYS2.SYSTEM_STATUS_INFO";
+    
+    try {
+   		console.log("SQL: " + sql);
+    	db.exec(sql, function(rs) {	  // Query the statement
+      		console.log (JSON.stringify(rs));
+			if(rs.length != 0) {
+				res.json(rs[0]);
+			} else res.json(500);
+        });
+        console.log ("Done");
+
+     } catch(e) {  // Exception handler
+     	console.log("Error: " + e);
+    	res.json(500);
+  	 }
+});
+
+// ---------------------------------------------------------------------------------------
+
+app.get('/system/asp', function(req, res, next) {
+    var result = {};
+    var sql = "SELECT system_asp_used FROM QSYS2.SYSTEM_STATUS_INFO";
+    
+    try {
+   		console.log("SQL: " + sql);
+    	db.exec(sql, function(rs) {	  // Query the statement
+      		console.log (JSON.stringify(rs));
+			if(rs.length != 0) {
+				res.json(rs[0]);
+			} else res.json(500);
+        });
+        console.log ("Done");
+
+     } catch(e) {  // Exception handler
+     	console.log("Error: " + e);
+    	res.json(500);
+  	 }
+});
+
+
+// ---------------------------------------------------------------------------------------
+
+// start server on the specified port and binding host
+appEnv.port = 19880;
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.port);
+});
+
+// ---------------------------------------------------------------------------------------
+
+process.on('SIGINT', function () {
+  console.log('SIGINT fired')
+  process.exit(1)
+});
+
+process.on('exit', function () {
+  console.log('exit fired');
+  console.log ("Close DB connection...");
+  db.close();
+});
+
+// ---------------------------------------------------------------------------------------
+
+
+
+
+
+
+
